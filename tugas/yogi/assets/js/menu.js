756e8d544db9f23352e254acf292ad7abc39f8e1
abdfaisol <abdfaisol.re@gmail.com>
2020-07-03 10:43:14 +0700

    request del from yogi

--- tugas/yogi/assets/js/menu.js
+++
@@ -1,7 +0,0 @@
-$('.contentsub').hide();
-$(".submenu ul li a").hover(function(){
-	var abc = $(this).attr('href');
-	$('.contentsub').hide();
-	$('.wrapmenu li').find(abc).show();
-	console.log(abc);
-})
